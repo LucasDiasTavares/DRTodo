593fa129fe505cf52ade68e985d2454def45f66d
Tavares <lucas.tavares1996@hotmail.com>
2020-03-01 17:44:39 -0300

    Colocando o debug no decouple

--- DRTodo/settings.py
+++ DRTodo/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 
-DEBUG = True
+DEBUG = config('DEBUG')
 
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
